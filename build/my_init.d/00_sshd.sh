f95c69ae1d1a64c68cc36be47d4ae72ded28b3a2
Reuben Avery <ravery2@its.jnj.com>
2015-05-09 22:05:05 -0400

    Making sshd enable run before regen host keys

--- build/my_init.d/00_sshd.sh
+++ build/my_init.d/00_sshd.sh
@@ -4,7 +4,6 @@ if [ $SSHD_ENABLED -eq 1 ]; then
   echo "### sshd service enabled"
   if [ -e /etc/service/sshd/down ]; then
     rm -f /etc/service/sshd/down
-    /etc/my_init.d/00_regen_ssh_host_keys.sh
   fi
 else
   echo "### sshd service disabled"
--- build/scripts/sshd.sh
+++ build/scripts/sshd.sh
@@ -6,4 +6,5 @@ set -x
 $minimal_apt_get_install openssh-client openssh-server
 cp -fRv /build/config/ssh/* /etc/ssh/
 
-/build/my_init.d/15_sshd_init.sh
+cp -f /build/my_init.d/00_sshd.sh /etc/my_init.d/
+mv /etc/my_init.d/00_regen_ssh_host_keys.sh /etc/my_init.d/01_regen_ssh_host_keys.sh
