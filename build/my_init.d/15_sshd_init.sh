2bf1f4abe219d54965dde0f312cb06156fcd2e9a
Reuben Avery <ravery2@its.jnj.com>
2015-05-09 21:30:43 -0400

    Re-executing regen host keys when sshd enabled

--- build/my_init.d/15_sshd_init.sh
+++ build/my_init.d/15_sshd_init.sh
@@ -3,6 +3,7 @@
 if [ $SSHD_ENABLED -eq 1 ]; then
   echo "### sshd service enabled"
   rm -f /etc/service/sshd/down
+  /etc/my_init.d/00_regen_ssh_host_keys.sh
 else
   echo "### sshd service disabled"
   touch /etc/service/sshd/down
